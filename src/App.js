e0e1044faccddfea314c6f723a82e4c244e09c1d
Simon Ljungström <simonlj8@gmail.com>
2023-06-06 20:01:14 +0200

    First step on my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Vinbanken</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Viner</a></li>
+            <li><a href="#">Recensioner</a></li>
+            <li><a href="#">Om oss</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <section className="hero">
+        <h2>Välkommen till Vinbanken</h2>
+        <p>Upptäck och dela dina favoritviner med andra!</p>
+      </section>
+
+      <section className="featured-drinks">
+        <h3>Utvalda drycker</h3>
+        <div className="drink-card">
+          <img src="drink1.jpg" alt="Dryck 1" />
+          <h4>Vin 1</h4>
+          <p>Ett fantastisk vin med en unik smak.</p>
+        </div>
+        <div className="drink-card">
+          <img src="drink2.jpg" alt="Dryck 2" />
+          <h4>Vin 2</h4>
+          <p>Ett uppfriskande vin perfekt för sommaren.</p>
+        </div>
+      </section>
+
+      <footer>
+        <p>Kontakta oss på info@vinbanken.se</p>
+      </footer>
     </div>
   );
 }
